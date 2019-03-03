36cd2706dc28d3085a4182515b2bafc5164a2528
ap-konovalov <sanches-zi@mail.ru>
2019-03-03 12:05:10 +0300

    Create lesson 2

---
+++ Leson_2/Leson_2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leson_2
+//
+//  Created by Aleksandr on 03.03.2019.
+//  Copyright © 2019 Aleksandr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
