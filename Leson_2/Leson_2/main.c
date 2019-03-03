c71d3361e467cc3ab547b9e02a538273ba6ac01e
ap-konovalov <sanches-zi@mail.ru>
2019-03-03 15:36:40 +0300

    Task 1 complete

--- Leson_2/Leson_2/main.c
+++ Leson_2/Leson_2/main.c
@@ -1,15 +1,25 @@
-//
-//  main.c
-//  Leson_2
-//
-//  Created by Aleksandr on 03.03.2019.
-//  Copyright © 2019 Aleksandr. All rights reserved.
-//
+//Коновалов А.П.
 
 #include <stdio.h>
 
+//функция переводит число number из десятичной системы счисления в двоичную
+void convertToBinary(int number)
+{
+    if (number >= 2)
+    {
+        convertToBinary(number/2);
+    }
+    printf("%d", number % 2);
+}
+
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    //Задание 1
+    // Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию.
+    convertToBinary(15);
+
     return 0;
 }
