898f3c6c65efe7417732ce908a1de0c8345c359a
ap-konovalov <sanches-zi@mail.ru>
2019-03-10 23:17:56 +0300

    Task 1,2 done

---
+++ Lesson_4/Lesson_4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lesson_4
+//
+//  Created by Aleksandr on 10.03.2019.
+//  Copyright © 2019 Aleksandr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
