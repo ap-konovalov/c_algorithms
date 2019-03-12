b5479f97d5850a2ae441be5b713f99fb58221f33
ap-konovalov <a.konovalov@dodopizza.com>
2019-03-12 20:11:30 +0300

    Create Lesson 5

---
+++ Lesson_5/Lesson_5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lesson_5
+//
+//  Created by Alexander Konovalov on 12/03/2019.
+//  Copyright © 2019 Alexander Konovalov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
