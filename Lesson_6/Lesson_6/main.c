7371ba5982cf6a104ede04a4a7e7ed711322c780
ap-konovalov <a.konovalov@dodopizza.com>
2019-03-16 13:46:50 +0300

    Task 1 done

--- Lesson_6/Lesson_6/main.c
+++ Lesson_6/Lesson_6/main.c
@@ -1,15 +1,35 @@
-//
-//  main.c
-//  Lesson_6
-//
-//  Created by Alexander Konovalov on 16/03/2019.
-//  Copyright © 2019 Alexander Konovalov. All rights reserved.
-//
+// Коновалов А.П.
+
 
 #include <stdio.h>
+#define STRINGSIZE 3
+
+//хеш-функция Дженкинса. Вычисляет хэш-функцию из строки str передаваемой в виде указателя на массив символов
+int hash(char *str)
+{
+    int hash,i;
+    for (i = hash = 0; i < STRINGSIZE;i++)
+    {
+        hash += (int)str[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+//    Задание 1
+//    Реализация хеш-функции Дженкинса. На вход функции подается
+//    строка, на выходе хэш строки.
+    
+    char string[STRINGSIZE] =  {'a','3','d'};
+    char *arr = string;
+    printf("%i\n", hash(arr));
+    
+
     return 0;
 }
