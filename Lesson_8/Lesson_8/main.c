23326c23e5317d9171434f3b8018bbb08da5a478
ap-konovalov <sanches-zi@mail.ru>
2019-03-23 11:46:23 +0300

    Lesson 8 created

--- Lesson_8/Lesson_8/main.c
+++ Lesson_8/Lesson_8/main.c
@@ -1,15 +1,9 @@
-//
-//  main.c
-//  Lesson_8
-//
-//  Created by Aleksandr on 23.03.2019.
-//  Copyright © 2019 Aleksandr. All rights reserved.
-//
+// Коновалов А.
+
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
     return 0;
 }
