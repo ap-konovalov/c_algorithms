cdc5e4325d6e1f7693297f77cbf362b7e14c48f4
ap-konovalov <sanches-zi@mail.ru>
2019-03-23 11:45:42 +0300

    Create Lesson 8

---
+++ Lesson_8/Lesson_8/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lesson_8
+//
+//  Created by Aleksandr on 23.03.2019.
+//  Copyright © 2019 Aleksandr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
